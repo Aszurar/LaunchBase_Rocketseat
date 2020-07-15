88f9302d12e8ce9d30e0282270892bac35252a48
Lucas de Lima <lms.souza39@gmail.com>
2020-07-15 18:56:21 -0300

    página do portfólio concluído com sucesso

---
+++ Works/aulasfrontend/portfolio.js
@@ -0,0 +1,11 @@
+const discordOverlay = document.querySelector('.discord-overlay');
+const discord = document.querySelector('.discord');
+
+
+discord.addEventListener("click", function(){
+    discordOverlay.classList.add("active");
+});
+
+document.querySelector(".close-modal").addEventListener("click", function(){
+    discordOverlay.classList.remove("active");
+});
