fff2ff8542d13512673c80248d7b4792cb3c1deb
Lucas de Lima <lms.souza39@gmail.com>
2020-07-16 23:24:07 -0300

    Página de projetos feita, e ainda em atualização

--- Works/aulasfrontend/portfolio.js
+++ Works/aulasfrontend/portfolio.js
@@ -1,6 +1,9 @@
 const discordOverlay = document.querySelector('.discord-overlay');
 const discord = document.querySelector('.discord');
 
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+const modal = document.querySelector('.modal');
 
 discord.addEventListener("click", function(){
     discordOverlay.classList.add("active");
@@ -8,4 +11,33 @@ discord.addEventListener("click", function(){
 
 document.querySelector(".close-modal").addEventListener("click", function(){
     discordOverlay.classList.remove("active");
+});
+
+cards.forEach(card => {
+        card.addEventListener("click", function(){
+        const typecard = card.getAttribute("id");
+        modalOverlay.classList.add("active");
+        modal.classList.add(typecard)
+    });
+});
+
+cards.forEach(card => {
+    const typecard = card.getAttribute("id");
+    
+    document.querySelector('.close-modal-overlay').addEventListener("click", function(){
+        modal.classList.remove(typecard);
+    });
+});
+
+document.querySelector('.close-modal-overlay').addEventListener("click", function() {
+    modalOverlay.classList.remove("active");
+    modal.classList.remove("maximize");
+});
+
+document.querySelector(".max-modal").addEventListener("click", function() {
+    modal.classList.add("maximize");
+});
+
+document.querySelector(".min-modal").addEventListener("click", function(){
+    modal.classList.remove("maximize");
 });
