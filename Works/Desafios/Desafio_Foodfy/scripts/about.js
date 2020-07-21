007bffef155d330d70a78217fed5334df4fb6ed7
Lucas de Lima <lms.souza39@gmail.com>
2020-07-21 19:45:34 -0300

    Atualizando desafios do módulo 2 e 1, além do começo do desafio Foodfy

---
+++ Works/Desafios/Desafio_Foodfy/scripts/about.js
@@ -0,0 +1,14 @@
+// const links = document.querySelectorAll('.tablink');
+
+
+// // links.forEach(link => {
+// //     link.addEventListener("click", function () {
+// //         link.classList.add("here");
+// //     });
+// // });
+
+// for (const link of links) {
+//     link.addEventListener("click", function () {
+//         link.classList.add("here");
+//     });
+// }
