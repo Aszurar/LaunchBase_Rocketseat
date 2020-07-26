6ad9020f2b07fb8c85f04e6be5d47a048fe263e4
Lucas de Lima <lms.souza39@gmail.com>
2020-07-25 21:52:24 -0300

    Página home, Sobre e de Receitas feitas com sucesso! Só algumas pequenas atualizações para terminar o desafio

---
+++ Works/Desafios/Desafio_Foodfy/scripts/index.js
@@ -0,0 +1,31 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+const modal = document.querySelector('.modal');
+
+cards.forEach(card => {
+    card.addEventListener("click", function () {
+        const imgcard = card.getAttribute("id");
+        const titlemodal = card.querySelector("h1").textContent;
+        const authormodal = card.querySelector("p").textContent;
+
+        modalOverlay.classList.add("active");
+        
+        modalOverlay.querySelector("img").src = `./assets/${imgcard}.png`;
+        modal.querySelector("h1").innerText = titlemodal;
+        modal.querySelector("p").innerText = authormodal;
+        
+    });
+});
+
+// cards.forEach(card => {
+//     const typecard = card.getAttribute("id");
+
+//     document.querySelector('.close-modal-overlay').addEventListener("click", function () {
+//         modal.classList.remove(typecard);
+//     });
+// });
+
+document.querySelector('.close-modal-overlay').addEventListener("click", function () {
+    modalOverlay.classList.remove("active");
+
+});
