ba05208c25ed9fd856237fcd8adab161335eb2c0
Lucas” <lms.souza39@gmail.com>
2020-08-21 21:57:44 -0300

    Desafio Refatorando Foodfy COMPLETO

--- Works/Desafios/Desafio_Foodfy/data.js
+++ Works/Desafios/Desafio_Foodfy/data.js
@@ -66,7 +66,7 @@ module.exports = [
         'Em uma frigideira ou assador coloque o óleo quando estiver quente frite até ficarem douradas.',
         'Para servir fica bonito com salada, ou abuse da criatividade.'
       ],
-      information: ''
+      information: 'Sirva com molho à parte.'
     },
 
     {
@@ -136,5 +136,94 @@ module.exports = [
         'açúcar de confeiteiro e corte em quadrados.'
       ],
       information: ''
+    },
+
+    {
+      image: 'https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2018/08/09/tortaholandesa.jpg',
+      title: 'Torta Alemã',
+      author: 'Rafaella Vagetti',
+      ingredients: [
+        '200 g de manteiga sem sal',
+        '1 xícara (chá) de açúcar',
+        '1 lata de creme de leite sem soro',
+        '1 pacote de bolacha maisena',
+        'leite, o quanto baste, para molhar a bolacha',
+        '1 lata de leite condensado, sabor chocolate (ou cobertura de sorvete)',
+      ],
+      preparation: [
+        'Coloque a manteiga e o açúcar na batedeira e bata até obter um creme bem fofo e liso.',
+        'Acrescente o creme de leite e bata rapidamente apenas para misturar.',
+        'Desligue a batedeira e reserve e separe um recipiente médio para montar o doce.',
+        'Acrescente um pouco de leite num prato fundo e molhe rapidamente algumas bolachas maisena no leite.',
+        'Forre o fundo do recipiente escolhido com uma camada de bolachas.',
+        'Acrescente uma camada do creme reservado sobre as bolachas.',
+        'Acrescente mais uma camada de bolachas molhadas no leite e repita o procedimento finalizando com a bolacha.',
+        'Cubra a última camada de bolachas com o leite condensado sabor chocolate (comprado pronto ou a cobertura).',
+        'Leve à geladeira por no mínimo 3 horas ou até que o doce fique bem gelado.',
+        'Retire o doce da geladeira e sirva a seguir.'
+      ],
+      information: 'É possível substituir o leite condensado de chocolate por cobertura de sorvete sabor chocolate, fica ótimo, por ser meio amargo quebra um pouco o doce. <br> Você sabia que a torta alemã, apesar do nome, é uma receita brasileira? <br><br>Dicas:<br> O creme de manteiga é super importante para que sua torta fique impecável, por isso, para obter o creme perfeito, é preciso que bata bem a manteiga junto com o açúcar com a batedeira, por mais ou menos 5 minutos. Isso faz com o creme fique super fofinho e aerado. <br> Na hora de acrescentar o creme de leite, é importante que ele esteja bem gelado. E é importante batê-lo bem junto ao creme, por uns 15 minutos, para que ele renda bem e fique liso e brilhante! <br> Em vez de leite condensado sabor chocolate, que tal fazer uma ganache deliciosa? <br> Na hora de fazer sua ganache, você pode adicionar uma colherzinha de rum ou essência de rum para dar um toque especial, fica uma delícia! <br> Além disso, é interessante inovar na decoração também: que tal fazer um chantilli super lindo e brilhante para finalizar? Sua torta vai ficar ainda mais incrível!'
+    },
+
+    {
+      image: 'https://lh3.googleusercontent.com/proxy/1HZPoWMSfR__GMF_HRMPKUqqLApD1XJAcXQjYzmvu-Wl1qtdNbs0KTAK4BGXMDwsACZwaZcky1pQEKtpZ35_TD6F3P3RFpK7px08ZVn7eLM',
+      title: 'Cupcake Gelado de Nutella',
+      author: 'Bruno de Oliveira',
+      ingredients: [
+        '1 copo de 350g de Nutella',
+        '1 pacote de biscoito maisena',
+        '1 pote de sorvete ao gosto pessoal, no caso desta receita: flocos',
+        '170g de chocolate meio amargo',
+        '170ml de creme de leite',
+      ],
+      preparation: [
+        'No liquidificador triture bem os biscoitos, coloque em uma tigela e misture com a nutella até virar uma massa.',
+        'Forre o fundo das forminhas de muffins e cubra com sorvete.',
+        'Leve para gelar. Derreta o chocolate e misture o creme de leite fazendo uma ganache. Desenforme os cupcakes, cubra com a ganache e sirva.',
+      
+      ],
+      information: 'Varie os sabores de sorvete'
+    },
+
+    {
+      image: 'https://www.saboresajinomoto.com.br/uploads/images/recipes/bobo-de-camarao-2.jpg',
+      title: 'Bobó de Camarão',
+      author: 'Lucas de Lima',
+      ingredients: [
+        '½ kg de camarão médio, limpo',
+        '1 kg de mandioca',
+        '200 ml de leite de coco (1 vidro pequeno)',
+        '4 colheres (sopa) de azeite de dendê',
+        '1 pimentão picado',
+        '1 cebola picada',
+        '3 dentes de alho picados',
+        '4 colheres (sopa) de azeite',
+        '4 colheres (sopa) de manteiga',
+        '2 colheres (sopa) de coentro picado',
+        'Sal e pimenta vermelha a gosto'
+      ],
+      preparation: [
+        'Descasque a mandioca, corte em pedaços grandes e coloque numa panela com água.',
+        'Leve a panela ao fogo alto e deixe cozinhar até a mandioca ficar bem macia.',
+        'Espete com um garfo para verificar.',
+        'Enquanto a mandioca cozinha, separe e pique os ingredientes pedidos na receita.',
+        'Numa frigideira, coloque o azeite e leve ao fogo para esquentar.',
+        'Junte o alho e refogue por 2 minutos, não deixe queimar.',
+        'Acrescente o camarão na frigideira, mexa bem e refogue por 3 minutos.',
+        'Desligue o fogo e reserve.',
+        'Coloque a manteiga numa panela e leve ao fogo para derreter.',
+        'Quando estiver bem quente, junte a cebola e o pimentão.',
+        'Mexa e refogue por 5 minutos. Desligue o fogo.',
+        'Quando a mandioca estiver cozida, desligue o fogo e escorra a água.',
+        'Coloque a mandioca ainda quente no copo do liquidificador, junte o leite de coco, o dendê e o refogado de cebola com pimentão.',
+        'Bata muito bem até formar uma massa lisa.',
+        'Cuidado para a tampa do liquidificador não abrir, pressione bem com um pano de prato dobrado.',
+        'Se for preciso, bata a mandioca em duas etapas.Tenha paciência, faça aos poucos que sempre dá certo.',
+        'Coloque o camarão refogado numa panela, junte o creme de mandioca e leve ao fogo.',
+        'Quando ferver, tempere com sal e pimenta.',
+        'Desligue o fogo e junte o coentro e Misture bem e sirva quente.'
+       
+      ],
+      information: 'O bobó dura até 2 dias na geladeira e 3 meses no congelador. Já o caldo de camarão dura 5 dias na geladeira e até 6 meses no congelador. <br> Para descongelar o bobó: deixe da noite para o dia na geladeira ou descongele direto na panela em fogo baixo, tampado, por cerca de 15 minutos, mexendo de vez em quando para não grudar no fundo.<br> Para descongelar no micro-ondas: leve o bobó tampado com um prato e aqueça em potência alta por 6 minutos, parando na metade do tempo para mexer. Uma forma de adiantar é preparar o caldo e cozinhar a mandioca no dia anterior ao preparo.'
     }
   ];
