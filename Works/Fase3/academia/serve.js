efad64c0455cdf5841e06a29915f043d80b1ad3d
Lucas” <lms.souza39@gmail.com>
2020-08-22 22:05:15 -0300

    O Começo da Fase 3

---
+++ Works/Fase3/academia/serve.js
@@ -0,0 +1,23 @@
+ const express = require('express');
+ const nunjucks = require('nunjucks');
+ const routes = require('./routes');
+ 
+ const server = express();
+
+ server.use(express.static("public"));
+ server.use(routes);
+
+//  .use, .set, dentre outros são middleware, que são configurações feitas no servidor desde quando criado até antes de ficar online,
+// "configurações do meio do caminho"
+
+server.set("view engine", "njk");
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+});
+
+server.listen(5010, function() {
+console.log("Server is running");
+});
