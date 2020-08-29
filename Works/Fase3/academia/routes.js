7cf4b8a18d30e627443d8fddecfed711e45eb856
Lucas” <lms.souza39@gmail.com>
2020-08-28 21:57:10 -0300

    Inserção de dados na Academia completa, construindo a validação das informações

--- Works/Fase3/academia/routes.js
+++ Works/Fase3/academia/routes.js
@@ -14,7 +14,23 @@ routes.get('/instructors/create', function(req, res){
 });
 
 routes.post('/instructors', function(req, res) {
-    return res.send("Recibido");
+    // req.body é o método usado para pegar dados do front end pelo método post
+    
+    // pegando todas chaves do objeto body que contem as chaves e os valores dos dados inseridos
+    const keys = Object.keys(req.body);
+    //Object é um construtor que criará um objeto, . keys é para pegar todas chaves do objeto passado como parâmetro
+    // Com isso, o Keys será um veto com todas chaves do body(objeto que possui os pares chaves e valor)
+
+    keys.forEach(key => {
+        // comparação para descobrir se alguma chave/input está vazio, se estiver, então mande uma mensagem ou renderize uma página
+        // informando para o usuário preencher todos dados
+        if(req.body[key] == ""){
+            return res.send("Please, fill all fields!");
+        }
+
+    });
+    
+    return res.send(req.body);
 })
 
 routes.get('/members', function(req, res) {
--- Works/Fase3/academia/serve.js
+++ Works/Fase3/academia/serve.js
@@ -4,6 +4,7 @@
  
  const server = express();
 
+ server.use(express.urlencoded({ extended: true }));
  server.use(express.static("public"));
  server.use(routes);
 
