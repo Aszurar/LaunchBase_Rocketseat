a964ebddc177afbbcb60d6f88b2befaf6a80f676
Lucas <lms.souza39@gmail.com>
2020-11-07 13:43:48 -0300

    Atualização responsividade completa menos na página de listagem dos alunos, páginas e seção dos alunos completos

--- Works/Desafios/desafio_4.1-4.7/public/scripts/script.js
+++ Works/Desafios/desafio_4.1-4.7/public/scripts/script.js
@@ -1,8 +1,6 @@
 const currentPath = location.pathname;
 const menuItems = document.querySelectorAll("header .links a");
 
-console.log(menuItems);
-
 menuItems.forEach(item => {
     if(currentPath.includes(item.getAttribute("href"))){
         item.classList.add("active");
