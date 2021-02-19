0bdb8796383ba22d6c922ae2b67fa09b6f84dc40
Lucas <lms.souza39@gmail.com>
2021-02-18 22:34:15 -0300

    :tada: CompleteHeader and Footer is complete./cgprog Now, Registrar's page is almost structure html finished

--- Works/Fase4/launchstore/src/routes.js
+++ Works/Fase4/launchstore/src/routes.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const routes = express.Router()
-
+const ProductController = require('./app/controllers/ProductController')
 
 routes.get('/', function (req, res){
     return res.render('layout')
+})
+
+// as funções do controle não são chamdas com () pois, com
+// os () elas executam imediatamente, e o que nos queremos
+// é que elas sóe xecuem quando aquela linha de código
+// for executada.
+routes.get('/products/create', ProductController.create)
+routes.get('ads/create', function (req, res){
+    return res.redirect('products/create')
 })
 
 module.exports = routes
